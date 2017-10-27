8aabc3328751fabe1a75a2159b7f36bd38e3cb09
Dan Capps <dcapps@mail.greenriver.edu>
2017-10-27 12:09:10 -0700

    Created ReminderMessage class with empty methods
    Created Checklist class

---
+++ src/main/java/model/ReminderMessage.java
@@ -0,0 +1,21 @@
+package model;
+
+public class ReminderMessage
+{
+    String message;
+
+    public void printMessage()
+    {
+
+    }
+
+    public void addNewMessage()
+    {
+
+    }
+
+    public void popUpMessage()
+    {
+
+    }
+}
